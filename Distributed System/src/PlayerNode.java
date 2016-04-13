038d4dab5758b991d2b810d2bfb34f1839e2a09b
Luke Toenjes <luke.toenjes@hotmail.com>
2016-04-13 14:57:34 -0600

    Added javadocs for setters and getters in the PlayerNode class

--- Distributed System/src/PlayerNode.java
+++ Distributed System/src/PlayerNode.java
@@ -42,89 +42,174 @@ public class PlayerNode {
 
 	}
 
+	/**
+	 * Gets the player's ID
+	 * @return playerID
+	 */
 	public int getPlayerID(){
 		return playerID;
 	}
 
+	/**
+	 * Sets the player hand to a particular pair of cards
+	 * @param c1
+	 * @param c2
+	 */
 	public void setHand(Card c1, Card c2){
 		hand[0] = c1;
 		hand[1] = c2;
 	}
 
+	/**
+	 * Sets the Big Blind flag for the player
+	 * @param b
+	 */
 	public void setBigBlind(boolean b){
 		bigBlind = b;
 	}
 
+	/**
+	 * Sets the Small Blind flag for the player
+	 * @param b
+	 */
 	public void setSmallBlind(boolean b){
 		smallBlind = b;
 	}
 	
+	/**
+	 * Gets the Big Blind flag of the player
+	 * @return bigBlind
+	 */
 	public boolean getBigBlind(){
 		return bigBlind;
 	}
 	
+	/**
+	 * Gets the Small Blind flag of the player
+	 * @return smallBlind
+	 */
 	public boolean getSmallBlind(){
 		return smallBlind;
 	}
 
+	/**
+	 * Gets a player's hand
+	 * @return hand
+	 */
 	public Card[] getHand(){
 		return hand;
 	}
 
+	/**
+	 * Gets the player's stack amount
+	 * @return
+	 */
 	public int getStack(){
 		return stack;
 	}
 
+	/**
+	 * Setst the beginTurn flag for a player
+	 * @param val
+	 */
 	public void setBeginTurn(boolean val){
 		beginTurn = val;
 	}
 	
+	/**
+	 * Gets the beginTurn flag for a player
+	 * @return beginTurn
+	 */
 	public boolean getBeginTurn(){
 		return beginTurn;
 	}
 	
+	/**
+	 * Sets the doneTurn flag for a player
+	 * @param val
+	 */
 	public void setDoneTurn(boolean val){
 		doneTurn = val;
 	}
 	
+	/**
+	 * Gets the doneTurn flag for a player
+	 * @return doneTurn
+	 */
 	public boolean getDoneTurn(){
 		return doneTurn;
 	}
 	
+	/**
+	 * Sets the turn of a player
+	 * @param val
+	 */
 	public void setTurn(boolean val){
 		turn = val;
 	}
 
+	/**
+	 * Gets the turn flag of a player
+	 * @return
+	 */
 	public boolean getTurn(){
 		return turn;
 	}
 
+	/**
+	 * Performs the bet for a player. Sets the appropriate amounts for that player
+	 * @param amount
+	 */
 	public void bet(int amount){
 		currentBetAmount += amount;
 		betPerTurn += amount;
 		stack -= amount;
 	}
 	
+	/**
+	 * Sets the folded flag for a player
+	 * @param b
+	 */
 	public void setFolded(boolean b){
 		folded = b;
 	}
 	
+	/**
+	 * Gets the folded flag for a player
+	 * @return folded
+	 */
 	public boolean getFolded(){
 		return folded;
 	}
 	
+	/**
+	 * Gets the currentBetAmount of a player
+	 * @return currentBetAmount
+	 */
 	public int getCurrentBetAmount(){
 		return currentBetAmount;
 	}
 	
+	/**
+	 * Sets the vote for a player
+	 * @param i
+	 */
 	public void setVote(int i){
 		vote = i;
 	}
 	
+	/**
+	 * Gets the vote value for a player
+	 * @return vote
+	 */
 	public int getVote(){
 		return vote;
 	}
 	
+	/**
+	 * Sets the stack value for a player
+	 * @param i
+	 */
 	public void setStack(int i){
 		stack = i;
 	}
